132eed16ce32e32095d2212c73b4635aacb89039
dql521 <douqianli@qq.com>
2020-02-14 21:02:17 +0800

    登录功能完成

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
+import moment from 'moment'
+import ElementUi from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.config.productionTip = false
+axios.defaults.baseURL = 'http://localhost:3000'
+Vue.prototype.$axios = axios
+Vue.use(ElementUi)
+Vue.filter('time', function (input) {
+  return moment(input).format('YYYY-MM-DD')
+})
 
 new Vue({
   router,
